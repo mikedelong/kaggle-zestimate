2c54f8d944dacc2de1de12012a20d203ec029e2b
Mike DeLong <mikedelong@outlook.com>
2017-09-14 16:47:47 -0400

    feature: added min-max scaling; added histograms of latitude and longitude.

--- data_exploration.py
+++ data_exploration.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import stateplane
+from sklearn.preprocessing import MinMaxScaler
 
 start_time = time.time()
 # set up logging
@@ -26,6 +27,16 @@ properties = pd.read_csv(properties_file, dtype={
     'propertyzoningdesc': np.str}, converters={
     'taxdelinquencyflag': lambda x: np.bool(True) if x == 'Y' else np.bool(False)})  # avoid mixed type warning
 
+logger.debug(properties['latitude'].isnull().sum())
+logger.debug(properties['longitude'].isnull().sum())
+if False:
+    min_max_scaler = MinMaxScaler(copy=True)
+    properties_latitude_mean = properties['latitude'].mean()
+    properties_longitude_mean = properties['longitude'].mean()
+    properties['latitude'].fillna(inplace=True, value=properties_latitude_mean)
+    properties['longitude'].fillna(inplace=True, value=properties_longitude_mean)
+    properties[['latitude', 'longitude']] = min_max_scaler.fit_transform(properties[['latitude', 'longitude']])
+
 train_df = pd.read_csv(training_file)
 # test_df = pd.read_csv("../input/sample_submission.csv")
 # test_df = test_df.rename(columns={'ParcelId': 'parcelid'})
@@ -36,8 +47,30 @@ t1 = len(train_df['parcelid'].unique())
 
 logger.debug('we have %d parcels of which %d are unique' % (t0, t1))
 
+t2 = train_df['parcelid'].duplicated(keep=False)
+t3 = train_df[t2 == True].sort_values(['parcelid'])
+# logger.debug('duplicated: %s' % t3)
+logger.debug('duplicated shape: %s' % (t3.shape,))
+
 train = train_df.merge(properties, how='left', on='parcelid')
 # test = test_df.merge(properties, on='parcelid', how='left')
+
+t4 = train[train['parcelid'].duplicated()]['parcelid'].index
+t5 = t4.shape
+t6 = train.drop(t4)
+t7 = t6.shape
+
+# let's get the mean and variance of the latitude and longitude
+logger.debug('train-longitude mean : %.2f std: %.2f min: %.2f max: %.2f' % (
+    train['longitude'].mean(), train['longitude'].std(), train['longitude'].min(), train['longitude'].max()))
+logger.debug('train-latitude mean : %.2f std: %.2f min: %.2f max: %.2f' % (
+    train['latitude'].mean(), train['latitude'].std(), train['latitude'].min(), train['latitude'].max()))
+logger.debug('properties-longitude mean : %.2f std: %.2f min: %.2f max: %.2f' % (
+    properties['longitude'].mean(), properties['longitude'].std(), properties['longitude'].min(), properties['longitude'].max()))
+logger.debug('properties-latitude mean : %.2f std: %.2f min: %.2f max: %.2f' % (
+    properties['latitude'].mean(), properties['latitude'].std(), properties['latitude'].min(), properties['latitude'].max()))
+
+logger.debug('%s' % train['latitude'].describe())
 
 # let's get some unique values:
 # for column_name in ['fips', 'regionidcity', 'regionidcounty', 'regionidzip', 'regionidneighborhood', 'storytypeid']:
@@ -62,7 +95,7 @@ if False:
         fmt='fips'), axis=1)
     logger.debug(
         train['t0'].head(20)
-)
+    )
 
 logger.debug('training data shape: %s' % (train.shape,))
 
@@ -90,7 +123,7 @@ logger.debug(train[column_name].describe())
 # visualize the percentile of the error on a map
 quantile = 0.995
 log_error_abs_quantile = train[column_name].abs().quantile(quantile)
-logger.debug('%s: at quantile level %f we have %f' % (column_name,quantile, log_error_abs_quantile))
+logger.debug('%s: at quantile level %f we have %f' % (column_name, quantile, log_error_abs_quantile))
 outliers = train.loc[abs(train[column_name]) > log_error_abs_quantile][['latitude', 'longitude', 'fips']]
 logger.debug('outliers shape : %s', (outliers.shape,))
 fig, ax = plt.subplots()
@@ -101,7 +134,7 @@ logger.debug('wrote file %s' % figure_filename)
 
 fig, axes = plt.subplots(ncols=2)
 logger.debug('%s min: %d max: %d' % (column_name, train[column_name].min(), train[column_name].max()))
-bins=40
+bins = 40
 train.hist(ax=axes[0], bins=bins, column=column_name)
 train.hist(ax=axes[1], bins=bins, column=column_name)
 axes[1].set_yscale('log')
@@ -122,6 +155,48 @@ ax.scatter(t0['latitude'], t0['longitude'], c=t0['fips'].apply(lambda x: colors[
 figure_filename = 'properties-latitude-longitude-fips.png'
 plt.savefig(figure_filename)
 logger.debug('wrote file %s' % figure_filename)
+
+# column_name = 'latitude'
+# fig, axes = plt.subplots(ncols=2, nrows=2)
+# logger.debug('train %s min: %d max: %d' % (column_name, train[column_name].min(), train[column_name].max()))
+# train.hist(ax=axes[0, 0], bins=50, column=column_name)
+# train.hist(ax=axes[0, 1], bins=50, column=column_name)
+# axes[0, 1].set_yscale('log')
+# logger.debug('properties %s min: %d max: %d' % (column_name, properties[column_name].min(), properties[column_name].max()))
+# properties.hist(ax=axes[1, 0], bins=50, column=column_name)
+# properties.hist(ax=axes[1, 1], bins=50, column=column_name)
+# axes[1, 1].set_yscale('log')
+# figure_filename = column_name + '-both.png'
+# plt.savefig(figure_filename)
+#
+# column_name = 'longitude'
+# fig, axes = plt.subplots(ncols=2, nrows=2)
+# logger.debug('train %s min: %d max: %d' % (column_name, train[column_name].min(), train[column_name].max()))
+# train.hist(ax=axes[0, 0], bins=50, column=column_name)
+# train.hist(ax=axes[0, 1], bins=50, column=column_name)
+# axes[0, 1].set_yscale('log')
+# logger.debug('properties %s min: %d max: %d' % (column_name, properties[column_name].min(), properties[column_name].max()))
+# properties.hist(ax=axes[1, 0], bins=50, column=column_name)
+# properties.hist(ax=axes[1, 1], bins=50, column=column_name)
+# axes[1, 1].set_yscale('log')
+# figure_filename = column_name + '-both.png'
+# plt.savefig(figure_filename)
+
+
+fig, axes = plt.subplots(ncols=2, nrows=2)
+column_name_1 = 'latitude'
+logger.debug('train %s min: %d max: %d' % (column_name_1, train[column_name_1].min(), train[column_name_1].max()))
+train.hist(ax=axes[0, 0], bins=50, column=column_name_1)
+logger.debug('properties %s min: %d max: %d' % (column_name_1, properties[column_name_1].min(), properties[column_name_1].max()))
+properties.hist(ax=axes[1, 0], bins=50, column=column_name_1)
+column_name_2 = 'longitude'
+logger.debug('train %s min: %d max: %d' % (column_name_2, train[column_name_2].min(), train[column_name_2].max()))
+train.hist(ax=axes[0, 1], bins=50, column=column_name_2)
+logger.debug('properties %s min: %d max: %d' % (column_name_2, properties[column_name_2].min(), properties[column_name_2].max()))
+properties.hist(ax=axes[1, 1], bins=50, column=column_name_2)
+figure_filename = '-'.join([column_name_1, column_name_2, 'both'])  + '.png'
+plt.savefig(figure_filename)
+
 
 column_name = 'taxvaluedollarcnt'
 fig, axes = plt.subplots(ncols=2)
